671ecb57e4dbc69151af3c2c9464dbc04200f9bd
mateoSikirca <mateo.sikirica33@gmail.com>
2020-12-18 18:50:37 +0100

    header

--- projekt/src/components/headerBar/index.js
+++ projekt/src/components/headerBar/index.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import styles from './style.module.css'
 
+const navTabs = ['Home', 'Blog', 'Contact', 'Components', 'Cart', 'Gaming']
+ 
 const HeaderBar = () => (
-  <main className={styles.container}>
-    <computerShop>COMPUTER SHOP</computerShop>
-    <line1></line1>
-    <line2></line2>
-    <home>HOME</home>
-    <blog>BLOG</blog>
-    <contact>CONTACT</contact>
-    <components>COMPONENTS</components>
-    <cart>CART</cart>
-    <gaming>GAMING</gaming>
-  </main>
+  <header className={styles.header}>
+      <ul className={styles.address}>
+          <li className={styles.title}>
+              COMPUTER SHOP
+          </li>
+      </ul>
+      
+      <ul className={styles.navigation}>
+          {navTabs.map(tab => {
+            if (tab === 'Home')
+            return (
+              <li className={styles.active}>
+                {tab}
+              </li>)
+            else
+              return <li>{tab}</li>
+            }
+          )}
+      </ul>
+  </header>
 )
 
 export default HeaderBar
