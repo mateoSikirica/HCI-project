9d5838bc7a156553e77dad71e99c422f43ca9353
ivanv <ivan.vili91@gmail.com>
2020-12-22 11:32:42 +0100

    finishing slider

--- projekt/src/components/slider/index.js
+++ projekt/src/components/slider/index.js
@@ -3,30 +3,22 @@ import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import styles from './style.module.css'
 
-
-
-const slideImages = [
-  'C:/Users/ivanv/HCI-project/projekt/src/images/setup2.jpg',
-  '../../images/setup3.jpg',
-  '../../images/setup2.jpg'
-];
-
 const Slideshow = () => {
     return (
       <div>
         <Slide easing="ease">
           <div className={styles.eachslide}>
-            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            <div className={styles.slideOne}>
               <span>Slide 1</span>
             </div>
           </div>
           <div className={styles.eachslide}>
-            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            <div className={styles.slideTwo}>
               <span>Slide 2</span>
             </div>
           </div>
           <div className={styles.eachslide}>
-            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+            <div className={styles.slideThree}>
               <span>Slide 3</span>
             </div>
           </div>
