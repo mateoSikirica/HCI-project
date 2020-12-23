aea49cff344921920283bb7c2e84bcf5854d1296
mateoSikirca <mateo.sikirica33@gmail.com>
2020-12-23 13:20:04 +0100

    homepage content

--- projekt/src/modules/PageContent/index.js
+++ projekt/src/modules/PageContent/index.js
@@ -7,15 +7,14 @@ import Third from '../../images/Third'
 import Fourth from '../../images/Fourth'
 
 const firstText = `
-Intel’s Rocket Lake flagship – presumably this is the top-end chip, because it’s an 8-core model.
+Intel’s Rocket Lake flagship – this is the top-end chip because it’s an 8-core model.
 `
- 
 const secondText = `
-The GeForce RTX™ 3080 delivers the ultra performance that gamers crave, powered by Ampere—NVIDIA’s 2nd gen RTX architecture.
+The GeForce RTX™ 3080 delivers the ultra performance that gamers crave.
 `
  
 const thirdText = `
-The all new RDNA powered Radeon RX 5700 XT for exceptional performance and High-fidelity gaming. 
+The new RDNA powered Radeon RX 5700 XT for high performance and gaming.
 `
 
 const fourthText = `
@@ -24,10 +23,10 @@ AMD just announced its new lineup of Ryzen 5000 series processors for desktops.
 
   const PageContent = () => (
     <section className={styles.pageContent}>
-      <ImageParagraph image={<First />} title="INTEL NEW 8 CORE CPU" text={firstText}/>
-      <ImageParagraph image={<Second />} title="NVIDIA GEFORCE RTX3080" text={secondText}/>
-      <ImageParagraph image={<Third />} title="AMD RADEON RX5700" text={thirdText}/>
-      <ImageParagraph image={<Fourth />} title="AMD 3 RYZEN 5000 PROCESSORS" text={fourthText}/>
+      <ImageParagraph image={<First />} title="INTEL NEW 8 CORE CPU" text={firstText} text2='AUTHOR: Ivan Vilić'/>
+      <ImageParagraph image={<Second />} title="NVIDIA GFORCE RTX3080" text={secondText} text2='AUTHOR: Ivan Vilić'/>
+      <ImageParagraph image={<Third />} title="AMD RADEON RX5700" text={thirdText} text2='AUTHOR: Mateo Sikirica'/>
+      <ImageParagraph image={<Fourth />} title="AMD 3 RYZEN 5000" text={fourthText} text2='AUTHOR: Mateo Sikirica'/>
     </section>)
 
 export default PageContent  
