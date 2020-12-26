ad36887a0071f19a6f46e872e479d4a3c2c7fe93
mateoSikirca <mateo.sikirica33@gmail.com>
2020-12-26 17:52:29 +0100

    homepage content

--- projekt/src/modules/PageContent/index.js
+++ projekt/src/modules/PageContent/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './style.module.css'
 import ImageParagraph from '../../components/ImageParagraph'
+import ImageP from '../../components/ImageP'
 import First from '../../images/First'
 import Second from '../../images/Second'
 import Third from '../../images/Third'
 import Fourth from '../../images/Fourth'
+import Brands from '../../images/Brands'
 
 const firstText = `
 Intel’s Rocket Lake flagship – this is the top-end chip because it’s an 8-core model.
@@ -30,6 +32,11 @@ AMD just announced its new lineup of Ryzen 5000 series processors for desktops.
       <section className={styles.blogBox}>
         <h2 className={styles.buttonBox}>Follow latest tech news on our blog</h2>
         <button className={styles.buttonBlog} type="button">Blog</button>
+      </section>
+      <section className={styles.componentsBox}>
+        <h2 className={styles.buttonBoxC}>Choose between top tech brands</h2>
+        <ImageP image={<Brands />}/>
+        <button className={styles.buttonComponents} type="button">Components</button>
       </section>
     </section>
     
