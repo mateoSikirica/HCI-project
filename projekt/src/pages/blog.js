8c140e538921104476ef24dbe6fb8709fb0e4714
ivanv <ivan.vili91@gmail.com>
2020-12-27 18:27:03 +0100

    adding blog page and fixing navigation

---
+++ projekt/src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from "react"
+import HeaderFooterLayout from "../layouts/headerFooter"
+
+const Blog = () => (
+    <HeaderFooterLayout activeTab="Blog">
+        
+    </HeaderFooterLayout>
+)
+
+export default Blog
