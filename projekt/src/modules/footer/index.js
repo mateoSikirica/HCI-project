3b378c8eb14eef21a475838044e4b9b2059b4acb
ivanv <ivan.vili91@gmail.com>
2020-12-30 18:58:12 +0100

    adding social media icons

--- projekt/src/modules/footer/index.js
+++ projekt/src/modules/footer/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import styles from './style.module.css'
-
 import NavigationBar from '../../components/navigationBar'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
  
   const Footer = ({activeTab}) => (
     <footer className={styles.footer}>
@@ -13,6 +19,20 @@ import NavigationBar from '../../components/navigationBar'
             <li>Ivan Vilić</li>
         </ul>
         <ul><NavigationBar /></ul>
+        <div className={styles.socialMedia}>
+          <a href="https://www.facebook.com" className={styles.facebook}>
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+          <a href="https://www.twitter.com" className={styles.twitter}>
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+          <a href="https://www.youtube.com" className={styles.youtube}>
+            <FontAwesomeIcon icon={faYoutube} size="2x" />
+          </a>
+          <a href="https://www.instagram.com" className={styles.instagram}>
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+        </div>
     </footer>
   )
  
